70c817adf021b022aa7f2ac0362bc8bcc219d961
Mert Kimyonşen <mertkimyonsen@hotmail.com>
2017-09-09 11:45:35 +0300

    Open Closed Principle

--- SolidPrinciples/OpenClosed.cs
+++ SolidPrinciples/OpenClosed.cs
@@ -6,7 +6,58 @@ using System.Threading.Tasks;
 
 namespace SolidPrinciples
 {
-    class OpenClosed
+    public class OpenClosed
     {
+        public OpenClosed()
+        {
+
+        }
+
+        /// <summary>
+        /// A class or function can open for development but a code change of 
+        /// </summary>
+        public void OpenClosedTechnic()
+        {
+
+            //We can inject method here * new implement
+            LogContext logContext = new LogContext(new EventLog());
+            logContext.WriteLogTo();
+        }
+    }
+
+    public interface ILogger
+    {
+        string WriteLog();
+    }
+
+    public class EventLog : ILogger
+    {
+        public string WriteLog()
+        {
+            return "Event Log";
+        }
+    }
+
+    public class DataBaseLog : ILogger
+    {
+        public string WriteLog()
+        {
+            return "DataBase log";
+        }
+    }
+
+    public class LogContext
+    {
+        private ILogger _logger;
+
+        public LogContext(ILogger logger)
+        {
+            this._logger = logger;
+        }
+
+        public string WriteLogTo()
+        {
+            return _logger.WriteLog();
+        }
     }
 }
